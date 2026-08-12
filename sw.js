eca10163eb52d14c224de2c89d3113b5655a4642
victorianotena <victoriano132@gmail.com>
2026-08-12 20:16:04 +0200

    El glosario decia que el boronio lleva pimiento rojo
    Describia la boronia andaluza, no el boronio de su madre, que es de berenjena y
    tomate y no lleva pimiento. Contradecia a la receta de la pagina de al lado.
    
    El resto del cambio es el sello de version, que cambia en cada generacion.

--- sw.js
+++ sw.js
@@ -1,7 +1,7 @@
 
 // Servicio que guarda la web en el movil para que funcione sin cobertura.
-var VERSION = 'plan-4f66e09a';
-var FICHEROS = ["index.html", "plan.html", "recetas.html", "basicos.html", "compra.html", "coste.html", "nutrientes.html", "progreso.html", "imprevistos.html", "guia.html", "estilo.css?v=4f66e09a", "app.js?v=4f66e09a", "plan.json?v=4f66e09a", "icono-192.png", "icono-512.png", "icono-apple.png", "favicon.png"];
+var VERSION = 'plan-7eeebd91';
+var FICHEROS = ["index.html", "plan.html", "recetas.html", "basicos.html", "compra.html", "coste.html", "nutrientes.html", "progreso.html", "imprevistos.html", "guia.html", "estilo.css?v=7eeebd91", "app.js?v=7eeebd91", "plan.json?v=7eeebd91", "icono-192.png", "icono-512.png", "icono-apple.png", "favicon.png"];
 
 self.addEventListener('install', function (e) {
   e.waitUntil(caches.open(VERSION).then(function (c) {
